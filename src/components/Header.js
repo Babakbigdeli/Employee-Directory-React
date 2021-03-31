a18aaf66009970a6dca78682ec374285a6646cc1
Babak Bigdeli <babakbigdeli@gmail.com>
2021-03-31 15:49:41 +0100

    Header added

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'; 
+import SearchBar from "./SearchBar";
+
+
+function Header(props) {
+        return (
+            <div className="header">
+                <h1>Employee Directory</h1>
+                <h3>Search for the emoployee using the search bar</h3>
+                />
+            </div>
+        )
+
+}
+
+export default Header;
