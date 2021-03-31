57add76292e581651b71a564679344fd35e501e8
Babak Bigdeli <babakbigdeli@gmail.com>
2021-03-31 16:24:53 +0100

    App.js updated accordingly

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
-import Main from "./components/Main";
 import Wrapper from "./components/Wrapper";
-import Header from "./components/Header";
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <Wrapper>
-        <Header />
-        <Main />
-      </Wrapper>
+      <Wrapper/>
     </div>
   );
 }
