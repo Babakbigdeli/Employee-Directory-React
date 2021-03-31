dbd35a9877e89486ecdfbcdb1e659cdaff8915c1
Babak Bigdeli <babakbigdeli@gmail.com>
2021-03-31 16:45:04 +0100

    Method for handling sort

--- src/components/SearchResData.jsx
+++ src/components/SearchResData.jsx
@@ -11,6 +11,37 @@ class SearchResData extends Component {
         filteredEmployees: [{}],
         order: "descend",
       };
+
+    //Method for handling sort
+
+    handleSort = (label) => {
+      //console.log(label);
+      if (this.state.order === "descend") {
+        this.setState({
+          order: "ascend",
+        });
+      } else {
+        this.setState({
+          order: "descend",
+        });
+      }
+
+      const comparedEmp = (empOne, empTwo) => {
+       
+
+        if (this.state.order === "ascend") {
+            return empOne[label].first.localeCompare(empTwo[label].first);
+        } else {
+            return empTwo[label].first.localeCompare(empOne[label].first);
+        }
+    
+        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare
+      };
+
+      const sortedEmployees = this.state.filteredEmployees.sort(comparedEmp);
+      this.setState({ filteredEmployees: sortedEmployees });
+      };
+
 };
 
 export default SearchResData;
