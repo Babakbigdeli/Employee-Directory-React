1169f0e632c4643db552d289036e4f1f9158f642
Babak Bigdeli <babakbigdeli@gmail.com>
2021-03-31 16:14:42 +0100

    SearchResData component created

---
+++ src/components/SearchResData.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import API from "../utils/API";
+import SearchResTable from "./SearchResTable";
+import SearchBar from "./SearchBar";
+
+class SearchResData extends Component {
+
+};
+
+export default SearchResData;
