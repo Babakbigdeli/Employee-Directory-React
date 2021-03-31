7371a0ecc8058092598982f041128f89bc5492f6
Babak Bigdeli <babakbigdeli@gmail.com>
2021-03-31 16:29:39 +0100

    EsLint Error ignored

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // using Axios to make http requests and fetch a random list of employees as sample. Wrapping it as a method and
 // export it as an object.
-
+// eslint-disable-next-line
 export default {
   getRandomEmployees: function() {
     return axios.get("https://randomuser.me/api/?results=50");
